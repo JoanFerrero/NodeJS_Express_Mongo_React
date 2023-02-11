5c9465af492a9098c584bfb6b7441f1ba903b4e6
unknown <joan.ferrero.montiel@gmail.com>
2023-02-11 17:52:11 +0100

    change name pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import NavBar from "./scenes/navbar"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./pages/navbar"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import store, { persistor }from "./redux/store"
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react'
-import FooterPage from "./scenes/footer";
+import FooterPage from "./pages/footer";
 import { PokemonProvider} from "./context/ProductsProvider"
 import React, { Suspense } from "react";
 function App() {
-  const HomePage = React.lazy(() => import('./scenes/homePage'))
-  const ShopPage = React.lazy(() => import('./scenes/shopPage'))
-  const LoginPage = React.lazy(() => import('./scenes/loginPage'))
-  const CardPage = React.lazy(() => import('./scenes/shopCard'))
+  const HomePage = React.lazy(() => import('./pages/homePage'))
+  const ShopPage = React.lazy(() => import('./pages/shopPage'))
+  const LoginPage = React.lazy(() => import('./pages/loginPage'))
+  const CardPage = React.lazy(() => import('./pages/shopCard'))
 
   return (
     <Provider store={store}>
@@ -18,7 +18,7 @@ function App() {
         <Suspense fallback={true}>
           <PokemonProvider>
             <NavBar />
-            <Router>
+            <BrowserRouter>
               <Routes>
                 <Route path="/" element={<HomePage />}/>
                 <Route path="/home" element={<HomePage />}/>
@@ -27,7 +27,7 @@ function App() {
                 <Route path="/contact" element={<HomePage />}/>
                 <Route path="/login" element={<LoginPage />}/>
               </Routes>
-            </Router>
+            </BrowserRouter>
             <FooterPage />
           </PokemonProvider>
         </Suspense>
